31c4bd2907c634a5aa0ad779129e10eb8c6d3d2e
96Diego <96DiegoTorres@gmail.com>
2023-08-11 14:25:46 -0300

    Initial commit

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,12 @@
+const nombre = document.getElementById("nombre");
+const apellido = document.getElementById("apellido");
+const email = document.getElementById("email");
+const password1 = document.getElementById("password1");
+const password2 = document.getElementById("password2");
+const terminos = document.getElementById("terminos");
+const boton = document.getElementById("regBtn");
+const a = password1.value;
+const b = password2.value;
 function showAlertSuccess() {
     document.getElementById("alert-success").classList.add("show");
 }
@@ -5,3 +14,19 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+boton.addEventListener("click",()=>
+{
+    if(nombre.checkValidity()&& apellido.checkValidity() && email.checkValidity() && password1.checkValidity() && password2.checkValidity() && terminos.checkValidity() && password1.value===password2.value)
+    {
+            showAlertSuccess();
+    }
+    else{showAlertError()};
+    console.log(password1,password2);
+}
+)
+
+
+
+
+
